0ea89ec3f2a3dedd4f9d3f990ec80916495d85aa
DeltaMaker <info@deltamaker.com>
2019-10-21 15:41:50 -0400

    Update copy-deltamaker-config.sh

--- scripts/copy-deltamaker-config.sh
+++ scripts/copy-deltamaker-config.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 # This script copies the deltamaker printer config and octoprint config files to
 #  the required locations
+#
+printer_cfg="/home/pi/printer.cfg"
+config_yaml="/home/pi/.octoprint/config.yaml"
 
-if [ -e ~/printer.cfg ]; then
-    echo "printer.cfg already exists"
+deltamaker_config="/home/pi/klipper/config/printer-deltamaker-r2-touch.cfg"
+octoprint_config="/home/pi/klipper/config/octoprint/config-deltamaker.yaml"
+if [ -f "$printer_cfg" ]; then
+    echo "Exiting: $printer_cfg already exists"
     exit -1
 fi
-#rm -f ~/printer.cfg 
-config_file="~/klipper/config/printer-deltamaker-r2-touch.cfg"
-echo "Installing $config_file in home directory"
-cp $config_file ~/
+#rm -f $printer_cfg 
+if [ -f "$deltamaker_config" ]; then
+    echo "Installing $deltamaker_config as $printer_cfg"
+    cp $deltamaker_config $printer_cfg
+else
+    echo "$deltamaker_config not found"
+fi
 
-octoprint_config="~/klipper/config/octoprint/config-deltamaker.yaml"
-echo $octoprint_config
-if [ -e $octoprint_config ]; then
-    echo "Installing $octoprint_config as ~/.octoprint/config.yaml"
-    cp $octoprint_config ~/.octoprint/config.yaml
+if [ -f "$octoprint_config" ]; then
+    echo "Installing $octoprint_config as $config_yaml"
+    cp $octoprint_config $config_yaml
+else
+    echo "$octoprint_config not found"
 fi
