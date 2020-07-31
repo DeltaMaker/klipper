02a67833be693914ddd1e198f9a2b76431f640e8
DeltaMaker <info@deltamaker.com>
2020-07-31 13:11:51 -0400

    snapshot

--- klippy/extras/save_gcode_offset.py
+++ klippy/extras/save_gcode_offset.py
@@ -17,7 +17,15 @@ class GCodeOffset:
             self.gcode.respond_info("Unable to parse gcode_offset in %s" % (self.name))
 
         self.printer.register_event_handler("klippy:ready", self.handle_ready)
-        self.gcode.register_command("SAVE_GCODE_OFFSET", self.cmd_SAVE_GCODE_OFFSET)
+        self.gcode.register_command("SAVE_GCODE_OFFSET", self.cmd_SAVE_GCODE_OFFSET, 
+                                    desc=self.cmd_SAVE_GCODE_OFFSET_help)
+
+    def home_command(self):
+        self.gcode.run_script_from_command("G28")
+        #cmd_G28 = self.gcode.register_command("G28", None)
+        #self.gcode.register_command("G28", cmd_G28)
+        #gcmd = self.gcode.create_gcode_command("G28", "G28", {})
+        #cmd_G28(gcmd)
 
     def handle_ready(self):
         self.gcode.respond_info("gcode_offset: X=%.3f Y=%.3f Z=%.3f" % tuple(self.gcode_offset))
@@ -25,22 +33,25 @@ class GCodeOffset:
             "SET_GCODE_OFFSET X=%.3f Y=%.3f Z=%.3f" % tuple(self.gcode_offset))
         # Home the toolhead if requested
         if GCodeOffset.home_when_ready:
-            self.gcode.run_script_from_command("G28")
             GCodeOffset.home_when_ready = False
+            home_command()
+            # this doesn't work
+            #gcmd = self.gcode.create_gcode_command("G28", "G28", {})
+            #self.gcode.cmd_G28(gcmd)
 
     cmd_SAVE_GCODE_OFFSET_help = "store g-code offset positions in printer config"
-    def cmd_SAVE_GCODE_OFFSET(self, params):
+    def cmd_SAVE_GCODE_OFFSET(self, gcmd):
         # Assumption: can get the current offset values from the GCodeParser object.
         # Need to confirm the homing_position array is a valid source for these values. 
         self.gcode_offset = self.gcode.homing_position[:3]
 
-        self.gcode.respond_info(
+        gcmd.respond_info(
             "gcode_offset: %.3f, %.3f, %.3f\n"
             "The SAVE_CONFIG command will update the printer config file\n"
             "with the above and restart the printer." % tuple(self.gcode_offset))
         configfile = self.printer.lookup_object('configfile')
         configfile.set(self.name, 'gcode_offset', "%.3f, %.3f, %.3f" % tuple(self.gcode_offset))
-        GCodeOffset.home_when_ready = (self.gcode.get_int('HOME', params, 0) != 0)
+        GCodeOffset.home_when_ready = (gcmd.get_int('HOME', 0) > 0)
 
 def load_config(config):
     return GCodeOffset(config)
