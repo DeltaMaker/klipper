62dfe9c2c8527485ea2b1c4715b3eac2e2fe02df
DeltaMaker <info@deltamaker.com>
2020-08-25 12:21:39 -0400

    refactoring 2t config
    work in progress

--- scripts/deltamaker-copy-dm2t-config.sh
+++ scripts/deltamaker-copy-dm2t-config.sh
@@ -2,16 +2,20 @@
 # This script copies the deltamaker printer config and octoprint config files to
 #  the required locations
 #
+dm_cfg="2t-einsy"
 printer_cfg="/home/pi/printer.cfg"
 config_yaml="/home/pi/.octoprint/config.yaml"
 
-deltamaker_config="/home/pi/klipper/config/printer-deltamaker-2t-einsy.cfg"
-octoprint_config="/home/pi/klipper/config/octoprint/config-deltamaker.yaml"
+deltamaker_config="/home/pi/printer-deltamaker-${dm_cfg}.cfg"
+octoprint_config="/home/pi/klipper/scripts/deltamaker-data/config-deltamaker.yaml"
 if [ -f "$printer_cfg" ]; then
     echo "Exiting: $printer_cfg already exists"
     exit -1
 fi
 #rm -f $printer_cfg 
+echo "Copying the DeltaMaker config files to home directory"
+cp --no-clobber /home/pi/klipper/config/printer-deltamaker-*.cfg /home/pi/
+
 if [ -f "$deltamaker_config" ]; then
     echo "Installing $deltamaker_config as $printer_cfg"
     cp $deltamaker_config $printer_cfg
