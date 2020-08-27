49bc766f1faf29a0e6e345b06ed7c0b4dca1b939
DeltaMaker <info@deltamaker.com>
2020-08-26 21:55:30 -0400

    fixed home_when_ready
    save_gcode_offset and G28 in ready event handler

--- klippy/extras/save_gcode_offset.py
+++ klippy/extras/save_gcode_offset.py
@@ -1,6 +1,6 @@
 # Save the current offset values set by the SET_GCODE_OFFSET command in printer config
 #
-# Copyright (C) 2019 Bob Houston <bob@deltamaker.com>
+# Copyright (C) 2019-2020 Bob Houston <bob@deltamaker.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
 #
@@ -19,25 +19,27 @@ class GCodeOffset:
         except:
             self.gcode.respond_info("Initializing gcode_offset in %s" % (self.name))
 
-        self.printer.register_event_handler("klippy:ready", self.handle_ready)
+        self.printer.register_event_handler("klippy:ready", self.restore_gcode_offset)
         self.gcode.register_command("SAVE_GCODE_OFFSET", self.cmd_SAVE_GCODE_OFFSET,
             desc=self.cmd_SAVE_GCODE_OFFSET_help)
 
-    def handle_ready(self):
-        self.gcode.respond_info("gcode_offset: X=%.3f Y=%.3f Z=%.3f" % tuple(self.gcode_offset))
-        self.gcode.run_script_from_command(
+    def restore_gcode_offset(self):
+        self.gcode.respond_info(
+            	"restore_gcode_offset: X=%.3f Y=%.3f Z=%.3f" % tuple(self.gcode_offset))
+        self.gcode.run_script(
             "SET_GCODE_OFFSET X=%.3f Y=%.3f Z=%.3f" % tuple(self.gcode_offset))
         # Home the toolhead if requested
         if GCodeOffset.home_when_ready:
             # homing when ready stopped working after recent git pull
-            #self.gcode.run_script_from_command("G28")
             GCodeOffset.home_when_ready = False
+            self.gcode.run_script("G28")
 
     cmd_SAVE_GCODE_OFFSET_help = "store g-code offset positions in printer config"
-    def cmd_SAVE_GCODE_OFFSET(self, params):
-        # Assumption: we can get the current offset values from the GCodeParser object.
+    def cmd_SAVE_GCODE_OFFSET(self, gcmd):
+        # Assumption: we can get the current offset values from the GCodeMove object.
         # Need to confirm the homing_position array is a reliable source for these values. 
-        self.gcode_offset = self.gcode.homing_position[:3]
+        gcode_move = self.printer.lookup_object('gcode_move')
+        self.gcode_offset = gcode_move.homing_position[:3]
 
         self.gcode.respond_info(
             "gcode_offset: %.3f, %.3f, %.3f\n"
@@ -45,7 +47,7 @@ class GCodeOffset:
             "with the above and restart the printer." % tuple(self.gcode_offset))
         configfile = self.printer.lookup_object('configfile')
         configfile.set(self.name, 'gcode_offset', "%.3f, %.3f, %.3f" % tuple(self.gcode_offset))
-        #GCodeOffset.home_when_ready = (self.gcode.get_int('HOME', params, 0) != 0)
+        GCodeOffset.home_when_ready = (gcmd.get_int('HOME', 0) != 0)
 
 def load_config(config):
     return GCodeOffset(config)
